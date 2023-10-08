d5ac809185eccae1fb4a32f913b7e833d07536f7
Nicosalaun <nico.salaun@gmail.com>
2023-10-08 21:42:31 +0200

    add my_cur.execute("insert into fruit_load_list values ('from streamlit')")

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
@@ -23,6 +24,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # write your own comment -what does the next line do? 
